0bb4a9a1137f059696b899c1bf698d23db19722d
nventuri97 <n.venturi3@studenti.unipi.it>
2019-10-19 22:01:28 +0200

    Let's start

--- ass4/src/com/company/MainClass.java
+++ ass4/src/com/company/MainClass.java
@@ -1,8 +1,8 @@
 package com.company;
 
-public class Main {
+public class MainClass {
 
     public static void main(String[] args) {
-	// write your code here
+	//iniziamo a scrivere qualcosa va
     }
 }
