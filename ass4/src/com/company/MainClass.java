7fee0d5b24d5fe63c99f0f2654b126c083a2c657
nventuri97 <n.venturi3@studenti.unipi.it>
2019-10-20 15:29:10 +0200

    Implementati Producer e Consumer, fix di problemi

--- ass4/src/com/company/MainClass.java
+++ ass4/src/com/company/MainClass.java
@@ -14,6 +14,7 @@ public class MainClass {
 
         //Genero casualmente un numero di consumatori tra 1 e 10
         int k=(int) (Math.random()*9)+1;
+        System.out.println(k);
 
         Producer produttore= new Producer(main_path, shared);
         Thread p=new Thread(produttore);
@@ -23,6 +24,11 @@ public class MainClass {
             Consumer consumatore=new Consumer(shared);
             Thread c= new Thread(consumatore);
             c.start();
+            try {
+                c.join();
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
         }
     }
 }
