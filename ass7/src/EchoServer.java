8fd69d1850982cc701168d7aefe7179d5d5328a8
nventuri97 <n.venturi3@studenti.unipi.it>
2019-11-16 18:13:56 +0100

    Server is goin' on

--- ass7/src/EchoServer.java
+++ ass7/src/EchoServer.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.Set;
 
 public class EchoServer {
     public static int DefaultPort=6798;
@@ -37,6 +41,72 @@ public class EchoServer {
             return;
         }
 
+        while (true) {
+            // Seleziona i canali pronti per almeno una delle operazioni di I/O tra quelli registrati con quel selettore
+            try {
+                selector.select();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                break;
+            }
+
+            Set<SelectionKey> readyKeys= selector.selectedKeys();
+            Iterator<SelectionKey> iterator=readyKeys.iterator();
+            while(iterator.hasNext()){
+                SelectionKey key=iterator.next();
+                //rimuove la chiave dal Selected set ma non dal registered set
+                iterator.remove();
+                try{
+                    if(key.isAcceptable()){
+                        ServerSocketChannel server=(ServerSocketChannel) key.channel();
+                        //Creo una active socket derivata dalla accept sulla passive socket su cui il server è in ascolto
+                        SocketChannel client=server.accept();
+                        System.out.println("Accepted connection from "+ client);
+                        //Setto a non-blocking
+                        client.configureBlocking(false);
+                        client.register(selector, SelectionKey.OP_READ);
+                    } else if(key.isReadable()){
+                        canBeRead(key);
+                    } else if(key.isWritable())
+                        canBeWrite(key);
+                } catch(IOException ioe){
+                    ioe.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //Metodo per la lettura del messaggio inviato dal client
+    public static void canBeRead(SelectionKey key) throws IOException{
+        SocketChannel client= (SocketChannel) key.channel();
+        ByteBuffer buff=ByteBuffer.allocate(MaxCapacity);
+        String cl_msg="";
+        int len=0;
+        while(true){
+            len=client.read(buff);
+            if(len==0)
+                break;
+            buff.flip();
+            while (buff.hasRemaining())
+                cl_msg+= StandardCharsets.UTF_8.decode(buff).toString();
+            buff.clear();
+        }
+
+        if(len==-1){
+            System.out.println("Aborted connection by client");
+            client.close();
+            key.cancel();
+        } else {
+            System.out.println("Message received from client " + client);
+            cl_msg+=" echod by server";
+            SelectionKey key2=key.interestOps(SelectionKey.OP_WRITE);
+            ByteBuffer response=ByteBuffer.wrap(cl_msg.getBytes());
+            key2.attach(response);
+
+        }
+    }
+
+    public static void canBeWrite(SelectionKey key) throws IOException{
         
     }
 }
