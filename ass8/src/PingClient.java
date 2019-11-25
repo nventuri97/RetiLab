53984cfa6588bb54081b80e10c6942ef8a39cc47
nventuri97 <n.venturi3@studenti.unipi.it>
2019-11-25 16:22:16 +0100

    Client development

--- ass8/src/PingClient.java
+++ ass8/src/PingClient.java
@@ -1,6 +1,53 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 
 public class PingClient {
+
     public static void main(String[] args){
-        
+        if(args.length!=2){
+            System.out.println("ERR Too much argument");
+            return;
+        }
+
+        try{
+            String server_name=args[0];
+            int port=Integer.parseInt(args[1]);
+            InetAddress address=InetAddress.getByName(server_name);
+            DatagramSocket sock=new DatagramSocket();
+            int i=0;
+            while(i<10) {
+                //costruisco la stringa per inviare il messaggio
+                String msg="PING"+(i+1);
+                byte[] data=msg.getBytes();
+                //compongo il datagram packet da inviare sul socket
+                DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+                //invio il datagram packet
+                sock.send(packet);
+
+                //devo aspettare la risposta del server
+                byte[] response=new byte[30];
+                DatagramPacket resp_packet=new DatagramPacket(response, 30);
+                //setto il timeout del socket
+                sock.setSoTimeout(2000);
+                sock.receive(resp_packet);
+                if(response==null)
+                    System.out.println("*");
+                else{
+                    msg=response.toString();
+                    System.out.println("msg");
+                }
+                i++;
+            }
+        } catch (UnknownHostException une){
+            System.out.println("ERR -arg 1");
+            return;
+        } catch (IOException ioe){
+            System.out.println("ERR -arg 2");
+            return;
+        }
     }
 }
--- ass8/src/PingServer.java
+++ ass8/src/PingServer.java
@@ -2,6 +2,11 @@
 public class PingServer {
 
     public static void main(String[] args){
+        if(args.length>2){
+            System.out.println("Too much argument");
+            return;
+        }
+
 
     }
 }
