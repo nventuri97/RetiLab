c1f9e618060228c66a0f716d71d8556b5ecea78b
nventuri97 <n.venturi3@studenti.unipi.it>
2019-12-14 15:36:01 +0100

    TimeServer, TimeClient implementation

--- ass10/src/TimeClient.java
+++ ass10/src/TimeClient.java
@@ -1,2 +1,40 @@
+import java.net.*;
+import java.io.*;
+import java.io.IOException;
+
 public class TimeClient {
+
+    public static void main(String args[]){
+        if(args.length!=2){
+            System.out.println("Usage: ERR -arg multicast_port ipGroup");
+            System.exit(1);
+        }
+
+        InetAddress group=null;
+        int port=0;
+        try{
+            group=InetAddress.getByName(args[1]);
+            port=Integer.parseInt(args[0]);
+
+            //Creo la socket e mi aggiungo al multicast
+            MulticastSocket clients=new MulticastSocket(port);
+            clients.joinGroup(group);
+            clients.setReuseAddress(true);
+
+            int i=0;
+            while(i<10) {
+                byte[] data = new byte[1024];
+                DatagramPacket packet = new DatagramPacket(data, data.length, group, port);
+                clients.receive(packet);
+                if(data.toString()==""){
+                    System.out.println("Server crashed");
+                    break;
+                }
+                System.out.println(data.toString());
+                i++;
+            }
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
 }
--- ass10/src/TimeServer.java
+++ ass10/src/TimeServer.java
@@ -1,2 +1,36 @@
+import java.io.*;
+import java.io.IOException;
+import java.net.*;
+import java.util.Calendar;
+
 public class TimeServer {
+    private static long interval=(long) (Math.random()*100)+1;
+
+    public static void main(String args[]){
+        if(args.length!=2){
+            System.out.println("Usage: ERR -arg multicast_port ipGroup");
+            System.exit(1);
+        }
+
+        try{
+            //costruisco l'InetAddress del server
+            InetAddress ias=InetAddress.getByName(args[1]);
+            //Creo la socket su cui si attaccherà il gruppo
+            int port=Integer.parseInt(args[0]);
+            DatagramSocket serversock=new DatagramSocket(port);
+            serversock.setReuseAddress(true);
+            System.out.println("TimeServer working");
+
+            while(true){
+                byte[ ] data= Calendar.getInstance().toString().getBytes();
+                DatagramPacket packet=new DatagramPacket(data, data.length, ias, port);
+                serversock.send(packet);
+                Thread.sleep(interval);
+            }
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        } catch (InterruptedException ie){
+            ie.printStackTrace();
+        }
+    }
 }
